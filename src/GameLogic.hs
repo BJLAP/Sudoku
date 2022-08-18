65bfe547d8356dfd7205935e7a59234d5e154358
Bert-Jan LappenschaarBerBer <bj_lappenschaar@hotmail.comB>
2022-08-18 19:48:21 +0200

    update algorithm

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -1,32 +1,44 @@
-
 module GameLogic(solve,inputGrid,createGrid) where
+
+import Data.List.Split
+import Data.List
+import Data.Maybe
 
 type Value = Int
 type HorPos = Int
 type VerPos = Int
+type ExcludedValues = [Value]
 type Location = (HorPos, VerPos)
 type Row = [Field]
 type InputValues = [[Value]]
 type Grid = [[Field]]
-type Field = (Location, Value)
+type Field = (Location, Value, ExcludedValues)
 
 input = [[1,0,3,0], [2,0,0,0],[0,0,0,0],[4,0,1,0]] :: InputValues
-inputGrid = createGrid input 0 
+input9by9 =[[7,9,0,0,0,3,0,0,0],[0,0,0,0,6,0,0,0,0],[8,0,0,3,0,0,0,7,6],[0,0,0,0,5,0,0,0,2],[0,0,5,4,1,8,0,0,0],[4,0,0,7,0,0,0,0,0],[6,1,0,0,9,0,0,0,8],[0,0,2,3,0,0,0,0,0],[0,0,9,0,0,0,0,5,4]] :: InputValues
+
+gridSize = 4
+
+field1 = ((0,1),1,[1,3,2]) :: Field
+field2 = ((0,3),0,[1,3]) :: Field
+inputGrid = calcExcludedValues (createGrid input 0)
+testRow = getRow field2 inputGrid
 
 solve :: Grid -> Grid
 solve grid | isComplete grid = grid
-solve grid = solve (updateGrid grid (findNextValues grid))
+solve grid = if grid /= updatedGrid then solve updatedGrid else grid
+    where updatedGrid = updateGrid grid (findNextValues grid)
 
-test = concat (updateGrid (createGrid input 0)  [Just ((0,0), 3)])
+test = concat (updateGrid (createGrid input 0)  [Just ((0,0), 3,[])])
 
 createGrid :: InputValues -> Int -> Grid
 createGrid [] _ = []
-createGrid (x:xs) init | init < 4 = createTuples x init 0 : createGrid xs (init + 1)
+createGrid (x:xs) init | init < gridSize = createTuples x init 0 : createGrid xs (init + 1)
 createGrid _ _ = []
 
 createTuples :: [Value] -> HorPos -> VerPos -> [Field]
 createTuples [] _ _ = []
-createTuples (x:xs) xpos ypos | ypos < 4 = ((xpos, ypos), x) : createTuples xs xpos (ypos + 1)
+createTuples (x:xs) xpos ypos | ypos < gridSize = ((xpos, ypos), x, []) : createTuples xs xpos (ypos + 1)
 createTuples _ _ _ = []
 
 updateGrid :: Grid -> [Maybe Field] -> Grid
@@ -37,54 +49,104 @@ updateRow row fields  = [ updateField fields fieldToUpdate | fieldToUpdate <- ro
 
 updateField :: [Maybe Field] -> Field -> Field
 updateField [] field = field
-updateField (Just x:xs) field = if fst x == fst field then x else updateField xs field
+updateField (Just x:xs) field = if getLocation x == getLocation field then x else updateField xs field
 updateField (Nothing:xs) field = updateField xs field
 
 
 isComplete :: Grid -> Bool
-isComplete grid = foldr ((&&).(>0).snd) True (concat grid)
--- a change
+isComplete grid = foldr ((&&).(>0).getValue) True (concat grid)
 
 findNextValues :: Grid -> [Maybe Field]
-findNextValues grid = map (findValue grid) (concat grid)  -- use map with partial function
+findNextValues grid = map (findValue gridWithExcludedValues) (concat gridWithExcludedValues) -- use map with partial function
+    where gridWithExcludedValues = calcExcludedValues grid
 
 findValue :: Grid -> Field -> Maybe Field
-findValue grid field | snd field == 0 = determineValue field (findRelatedFields field grid)
+--findValue grid field | getValue field == 0 = determineValue field (findRelatedFields field grid) 1
+findValue grid field | getValue field == 0 = determineValueGrid grid field 1
 findValue _ _ = Nothing
-
-groupVertical :: Grid -> Grid
-groupVertical [[],[],[],[]] = []
-groupVertical (a:b:c:d:ds) = [head a, head b, head c, head d] : groupVertical [tail a,tail b,tail c,tail d]
 
 groupSquares :: [[a]] -> [[a]]
 groupSquares [[]] = []
---groupSquares grid = concat (map (splitList 4) (map (combineNth 4) (splitList 8 (concat grid))))
-groupSquares grid = concatMap (splitList 4 . combineNth 4) (splitList 8 (concat grid))
+groupSquares grid = concatMap (map concat . transpose) (chunksOf (getSqrt gridSize) (map (chunksOf (getSqrt gridSize)) grid))
 
-splitList :: Int -> [a] -> [[a]]
-splitList _ [] = []
-splitList n list | n < 1 || n > length list = []
-splitList n list = take n list : splitList n (reverse(take (length list-n) (reverse list))) 
-
-combineNth :: Int -> [a] -> [a]
-combineNth  _ [] = []
-combineNth n list | length list <= n  = []
-combineNth n (x:xs) = x : xs!!(n-1) : combineNth n xs
+getSqrt :: Int -> Int
+getSqrt x = round(sqrt (fromIntegral x))
 
 findRelatedFields :: Field -> Grid -> [Field]
-findRelatedFields field grid = concatMap (listContainsField field) grid ++ concatMap (listContainsField field) (groupVertical grid) ++  concatMap (listContainsField field) (groupSquares grid)
+findRelatedFields field grid = concatMap (listContainsField field) grid ++ concatMap (listContainsField field) (transpose grid) ++  concatMap (listContainsField field) (groupSquares grid)
 
 listContainsField :: Field -> [Field] -> [Field]
-listContainsField field fields = if foldr ((||).(== fst field).fst)False fields then fields else []
+listContainsField field fields = if foldr ((||).(== getLocation field).getLocation)False fields then fields else []
 
-determineValue :: Field -> [Field] -> Maybe Field
-determineValue _ [] = Nothing
-determineValue field fields | hasValue 1 fields &&  hasValue 2 fields &&  hasValue 3 fields  = Just (fst field, 4)
-determineValue field fields | hasValue 1 fields &&  hasValue 2 fields &&  hasValue 4 fields  = Just (fst field, 3)
-determineValue field fields | hasValue 1 fields &&  hasValue 3 fields &&  hasValue 4 fields  = Just (fst field, 2)
-determineValue field fields | hasValue 2 fields &&  hasValue 3 fields &&  hasValue 4 fields  = Just (fst field, 1)
-determineValue field fields = Nothing
+determineValue :: Field -> [Field] -> Int -> Maybe Field
+determineValue _ [] _ = Nothing
+determineValue x xs y | y > 0 && y <= gridSize = if isValidValue y xs then Just (getLocation x, y, getExcludedValues x) else determineValue x xs (y + 1)
+determineValue _ _ _ = Nothing
+
+-- use excluded values arrays of all related fields (use grid instead of [Field])
+-- Todo: correct calcExcludedValues for Fields that have a value (ie. test with calcExcludedValues inputGrid
+-- Expected: a field with a value has as excluded values all other values.
+determineValueGrid :: Grid -> Field -> Int -> Maybe Field
+determineValueGrid [[]] _ _ = Nothing
+determineValueGrid grid field y | isJust maybeValueRow = maybeValueRow
+                                | isJust maybeValueVerticalRow = maybeValueVerticalRow
+                                | isJust maybeValueSquare = maybeValueSquare
+                                | otherwise = Nothing
+    where maybeValueRow = determineValueRow field y (getRow field grid)
+          maybeValueVerticalRow = determineValueRow field y (getRow field (transpose grid))
+          maybeValueSquare = determineValueRow field y (getRow field (groupSquares grid))
+
+-- use excluded values arrays of all related fields (use grid instead of [Field])
+determineValueRow :: Field -> Int -> Row -> Maybe Field
+determineValueRow _ _ [] = Nothing
+-- Strategy 1 for solving Sudoku: if all possible values but 1 are excluded for a field (because related fields contain this value), this is the valid value
+determineValueRow field y row | y > 0  && y <= gridSize && length(getExcludedValues field) == (gridSize -1 ) = if y `notElem` getExcludedValues field then Just (getLocation field, y, getExcludedValues field) else determineValueRow field (y + 1) row
+-- Strategy 2 for solving Sudoku: if a value is excluded in all related fields (i.e. a square or row) than this must be the valid value for this field
+determineValueRow field y row | y > 0  && y <= gridSize = if valueIsExcludedInRow y (removeValue field row) then Just (getLocation field, y, getExcludedValues field) else determineValueRow field (y + 1) row
+determineValueRow _ _ _  = Nothing
+
+valueIsExcludedInRow :: Int -> [Field] -> Bool 
+valueIsExcludedInRow _ [] = False
+valueIsExcludedInRow x xs =  all (elem x . getExcludedValues) xs
+
+isValidValue :: Int -> [Field] -> Bool
+isValidValue _ [] = False
+isValidValue x xs = containsAllValues (removeValue x [1..gridSize]) xs
 
 hasValue :: Int -> [Field] -> Bool
 hasValue _ [] = False 
-hasValue x fields = foldr ((||).(== x).snd) False fields
+hasValue x fields = foldr ((||).(== x).getValue) False fields
+
+containsAllValues :: [Int] -> [Field] -> Bool
+containsAllValues _ [] = False
+containsAllValues xs ys = all (\ z -> z `elem` map getValue ys) xs
+
+removeValue :: (Eq a) => a -> [a] -> [a]
+removeValue _ []              = []
+removeValue x (y:ys) | x == y = removeValue x ys
+                     | otherwise  = y : removeValue x ys
+
+getLocation :: Field -> Location
+getLocation (l,_,_) = l
+
+getValue :: Field -> Value
+getValue (_,v,_) = v
+
+getExcludedValues :: Field -> ExcludedValues
+getExcludedValues (_,_,e) = e
+
+calcExcludedValues :: Grid -> Grid
+calcExcludedValues grid = map (calcExcludedValuesRow grid) grid 
+
+calcExcludedValuesRow :: Grid -> Row -> Row
+calcExcludedValuesRow grid = map (calcExcludedValuesField grid)
+
+calcExcludedValuesField :: Grid -> Field -> Field
+calcExcludedValuesField grid field = (getLocation field, getValue field, removeValue (getValue field) (getDistinctValues(findRelatedFields field grid)))
+
+getDistinctValues :: [Field] -> [Int]
+getDistinctValues fields = removeValue 0 (nub(map getValue fields))
+
+getRow :: Field -> [Row] -> Row
+getRow _ [] = []
+getRow field (r:rs) = if field `elem` r then r else getRow field rs 
